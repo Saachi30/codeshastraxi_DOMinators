29047aeab245ccc1254d7788c68b6b2187d0d5cd
Khushi Parekh <knup0511@gmail.com>
2025-04-06 07:25:55 +0530

    ui

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const LoadingScreen = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-900 to-purple-900 flex items-center justify-center">
+    <div className="min-h-screen bg-gradient-b from-[#99BC85]  to-[#7FA56D] flex items-center justify-center">
       <div className="text-center">
         <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-400 mx-auto mb-4"></div>
-        <h2 className="text-2xl font-semibold text-white">Loading SecureVote</h2>
+        <h2 className="text-2xl font-semibold text-black">Loading SecureVote</h2>
         <p className="text-blue-200 mt-2">Setting up your secure voting environment...</p>
       </div>
     </div>
